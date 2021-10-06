acd70b24cdf984a15f97087d92172c0f367fc3f1
Nik Kranjc Gregoric <nik.kranjc.gregoric@gmail.com>
2021-10-06 10:09:21 +0200

    a

--- nekaj/nekaj/Program.cs
+++ nekaj/nekaj/Program.cs
@@ -10,8 +10,47 @@ namespace nekaj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("clen 2 " + ClenZap(2));
             Console.ReadLine();
+        }
+
+        static int ClenZap(int n)
+        {
+            if(n == 1 ||n == 2)
+            {
+                return 2;
+            }
+            return ClenZap(n - 2) * ClenZap(n - 1) - 1;
+        }
+        
+        static int Fib(int n)
+        {
+            //rekurzivno izrečuna n-to fibonacijevo število
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            return (Fib(n-1) + Fib(n - 2));
+        }
+        
+
+        static int PotencaIterativno(int n)
+        {
+            int potenca = 1;
+            for(int k = 1; k<=n; k++)
+            {
+                potenca = potenca * 2;
+            }
+            return potenca;
+        }
+
+        static int PotencaRekurzivno(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return 2 * PotencaRekurzivno(n - 1);
         }
     }
 }
