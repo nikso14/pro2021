3a3f8587c94e9f44eb9097bbb58c8ef0488102db
Nik Kranjc Gregoric <nik.kranjc.gregoric@gmail.com>
2021-10-08 12:51:06 +0200

    dod palindrom

---
+++ Palindrom/Palindrom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(JePalindrom("abba"));
+            Console.ReadLine();
+        }
+
+        static bool JePalindrom(String niz)
+        {
+            if (niz.Length == 1 || niz.Length == 0)
+            {
+                return true;
+            }
+            else if (niz[0] == niz[niz.Length-1])
+            {
+               return JePalindrom(niz.Substring(1, niz.Length - 2));
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
