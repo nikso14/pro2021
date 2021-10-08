3e9159e9e8962c31fc7faf625451c9719c687e41
Nik Kranjc Gregoric <nik.kranjc.gregoric@gmail.com>
2021-10-08 12:33:49 +0200

    hanojski

---
+++ HanojskiStolpi/HanojskiStolpi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HanojskiStolpi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Premakni(3, "a", "c", "b");
+            Console.WriteLine();
+        }
+
+        static void Premakni(int n, string začetni, string končni, string pomožni)
+        {
+            if(n == 1)
+            {
+                Console.WriteLine("premakni iz " + začetni + " na " + končni);
+                return;
+            }
+            Premakni(n - 1, začetni, pomožni, končni);
+            Console.WriteLine("Premakni iz " + začetni + " na " + končni);
+            Premakni(n - 1, pomožni, končni, začetni);
+        }
+       
+    }
+}
